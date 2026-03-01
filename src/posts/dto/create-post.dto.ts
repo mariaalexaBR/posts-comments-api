368778a1e5b789e972d04dbcdac6b2e0c8ed9445
Alexandra Brenes <mariaalexandra.brenes@gmail.com>
2026-03-01 11:12:30 -0600

    implement post DTO validation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10)
+  body: string;
+
+  @IsString()
+  @IsNotEmpty()
+  author: string;
+}
