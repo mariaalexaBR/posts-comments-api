afb3b52f26965f683f52c664e009d261b3b29921
Alexandra Brenes <mariaalexandra.brenes@gmail.com>
2026-03-01 10:38:49 -0600

    create posts module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
@@ -8,6 +9,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
+    PostsModule,
   ],
 })
 export class AppModule {}
