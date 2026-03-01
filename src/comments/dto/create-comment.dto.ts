25504fc4639f20571a880903132fd812dbea8b5f
Alexandra Brenes <mariaalexandra.brenes@gmail.com>
2026-03-01 13:54:02 -0600

    implement comment schema and DTO validation

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,20 @@
-export class CreateCommentDto {}
+import { IsNotEmpty, IsString, IsEmail, IsMongoId } from 'class-validator';
+
+export class CreateCommentDto {
+
+  @IsMongoId()
+  @IsNotEmpty()
+  postId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  body: string;
+}
---
+++ src/comments/schemas/comment.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument, Types } from 'mongoose';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema({ timestamps: true })
+export class Comment {
+  @Prop({ type: Types.ObjectId, ref: 'Post', required: true })
+  postId: Types.ObjectId;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  body: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
