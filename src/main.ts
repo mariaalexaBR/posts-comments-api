63b4f650a0c89164b3b3cbf3f17c15ffb688aa9e
Alexandra Brenes <mariaalexandra.brenes@gmail.com>
2026-03-01 22:52:13 -0600

    enable CORS for Angular frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200', // Angular
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
